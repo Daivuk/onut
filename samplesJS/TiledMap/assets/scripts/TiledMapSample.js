5d5683ee731afa839a4f411c287565bb4dc60848
Daivuk <daivuk@gmail.com>
2016-09-14 09:18:10 -0400

    JS TiledMap fo realsy

--- samplesJS/TiledMap/assets/scripts/TiledMapSample.js
+++ samplesJS/TiledMap/assets/scripts/TiledMapSample.js
@@ -1,11 +1,5 @@
 var tiledMap = getTiledMap("sample.tmx");
 
-var cliftSideId = tiledMap.getTileAt(2, 4, 0);
-tiledMap.setTileAt(0, 0, 0, cliftSideId);
-tiledMap.setTileAt(0, 1, 0, cliftSideId);
-tiledMap.setTileAt(0, 2, 0, cliftSideId);
-tiledMap.setTileAt(0, 3, 0, cliftSideId);
-
 function render() {
     tiledMap.render();
 }
