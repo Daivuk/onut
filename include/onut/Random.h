87d55bfcca747e94d093b77b221d25ef98fd4a05
davidstl <daivuk@gmail.com>
2020-03-01 18:13:21 -0500

    Fixed shuffle vector compile error on mac

--- include/onut/Random.h
+++ include/onut/Random.h
@@ -60,7 +60,7 @@ namespace onut
     template<typename T>
     void shuffle(std::vector<T>& vec)
     {
-        std::random_shuffle(vec.begin(), vec.end());
+        std::shuffle(std::begin(vec), std::end(vec));
     }
 
     Vector2 randCircle(const Vector2& center, float radius);
